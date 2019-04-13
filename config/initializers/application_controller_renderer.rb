7d7716609449d6bc07372547023176d4012c4490
kazutofuruta <19940718fk@gmail.com>
2019-04-13 16:48:21 +0900

    [Finish]3章sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'a51a74938d4ad9b765a9f9e4f75a280674a55509ab78e0c96c01d63e4b53529a73b066020d18b2ca52e7d80ff4567a67fc778b4abd6c65f696f90c3ccef68302'
